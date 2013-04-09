315da87e6122c9999a3c552340b01a3ab1a8786c
Michael Murray <michael@freeassoc.net>
2013-04-08 20:11:23 -0700

    added lat/long to hotel, moved HTTP basic auth and creds under v1 so we can have different auth scheme and creds for different versions of API

--- app/controllers/v1/base_controller.rb
+++ app/controllers/v1/base_controller.rb
@@ -1,3 +1,13 @@
+include ActionController::HttpAuthentication::Basic::ControllerMethods
+
 class V1::BaseController < ApplicationController
+
+  before_filter :http_basic_authenticate
+
+  def http_basic_authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "HIAPI" && password == "$tockh0lm"
+    end
+  end
 
 end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,8 +2,15 @@ class Hotel
   include Mongoid::Document
   include Mongoid::Timestamps
   include Mongoid::Slug
-  
+
   field :name, type: String
   slug :name
-  
+
+  field :location, :type => Array
+
+  index(
+    { location: "2d" },
+    { min: -200, max: 200}
+   )
+
 end
