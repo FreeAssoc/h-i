d52cf904c768897c03e125b428a2a299442811c4
Michael Murray <michael@freeassoc.net>
2013-04-09 20:27:53 -0700

    WIP one more

--- app/models/contact_info.rb
+++ app/models/contact_info.rb
@@ -1,7 +1,9 @@
 class ContactInfo
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :name, type: String
   field :email, type: String
   field :phone1, type: String
   field :phone2, type: String
+
 end
